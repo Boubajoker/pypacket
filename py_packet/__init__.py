2ff7458a38454352f2e4ce57ef47a57a97cb78ad
Boubajoker <mohanathas.leo@gmail.com>
2021-10-27 17:49:38 +0200

    no info given

--- py_packet/__init__.py
+++ py_packet/__init__.py
@@ -9,4 +9,4 @@ class txt:
         file.write('test : true')
         file.close()
 
-print("Hello from the pypacket community !! get started at https://www.pypacket.dev/. Happy coding :)!")
+print("Hello from the pypacket community !! get started at https://boubajoker.github.io/pypacket/. Happy coding :)!")
