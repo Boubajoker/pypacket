5ace28b3f172700dd92faa6bc69120364c77abdc
Boubajoker <mohanathas.leo@gmail.com>
2021-10-29 16:29:57 +0200

    Vue.js support added for docs !!

---
+++ docs/JS/app.js
@@ -0,0 +1,12 @@
+const app = new Vue({
+    el: '#app_container',
+    data: {
+        q1: "What is pypacket ?",
+        command: "pip install pypacket",
+        p1: "A python module that can make files packet for send a request file to a server or create a log.",
+        p2: "to create a json packet we are using the class json. and the function 'create_json_packet()' then the code look's like this : ",
+        p3: "To install pypacket module please follow the following steps :",
+        li1 : "open your terminal,",
+        li2: "type the following command:"
+    }
+});
