53a6a04fe4b23f72aa0899447f9c47ab26a2cf7f
Boubajoker <mohanathas.leo@gmail.com>
2021-11-01 15:29:10 +0100

    fixed some bug :bug:  and added some things you love :sunglasses:

--- docs/JS/app.js
+++ docs/JS/app.js
@@ -1,12 +1,12 @@
-const app = new Vue({
-    el: '#app_container',
-    data: {
-        q1: "What is pypacket ?",
-        command: "pip install pypacket",
-        p1: "A python module that can make files packet for send a request file to a server or create a log.",
-        p2: "to create a json packet we are using the class json. and the function 'create_json_packet()' then the code look's like this : ",
-        p3: "To install pypacket module please follow the following steps :",
-        li1 : "open your terminal,",
-        li2: "type the following command:"
-    }
-});
+const txtAnim = document.querySelector("h1")
+
+new Typewriter(txtAnim, {
+    deleteSpeed: 50,
+    loop: true,
+})
+.typeString("Welcome To pypacket Documentation !")
+.pauseFor(3000)
+.typeString('Create <strong style="color: red;">youre packet files !</strong>')
+.pauseFor(3000)
+.deleteChars(32)
+.start()
