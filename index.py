fde74f325a281ae8aae5d7418d7f08e416ca8fc2
Boubajoker <mohanathas.leo@gmail.com>
2021-11-06 16:00:57 +0100

    Deleted server setup.

--- index.py
+++
@@ -1,8 +0,0 @@
-import cgi
-
-with open("server.html", "r") as f:
-    html_content = f.read()
-    f.close()
-
-print("Content-type: text/html; charset=utf-8\n")
-print(html_content)
--- server/JS/app.js
+++
@@ -1,9 +0,0 @@
-const btn_ctrl_1 = document.querySelector("#btn_control_1");
-const btn_ctrl_2 = document.querySelector("#btn_control_2")
-
-btn_ctrl_1.addEventListener('click', () => {
-    window.alert("To shutdown the server close your terminal.")
-})
-btn_ctrl_2.addEventListener('click', () => {
-    window.open("server/pypacket_server.zip")
-})
